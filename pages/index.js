520872e39637b3f64a2d704318733f774a439d95
Pham Hong Thai <hongthaipro@gmail.com>
2022-03-30 22:01:48 +0700

    Querying the Blockchain

--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,31 @@ import * as fcl from "@onflow/fcl";
 
 export default function Home() {
   const [user, setUser] = useState({ loggedIn: null });
+  const [name, setName] = useState("");
 
   useEffect(() => fcl.currentUser.subscribe(setUser), []);
+
+  const sendQuery = async () => {
+    const profile = await fcl.query({
+      cadence: `
+        import Profile from 0xProfile
+
+        pub fun main(address: Address): Profile.ReadOnly? {
+          return Profile.read(address)
+        }
+      `,
+      args: (arg, t) => [arg(user.addr, t.Address)],
+    });
+
+    setName(profile?.name ?? "No Profile");
+  };
 
   const AuthedState = () => {
     return (
       <div>
         <div>Address: {user?.addr ?? "No Address"}</div>
+        <div>Profile Name: {name ?? "--"}</div>
+        <button onClick={sendQuery}>Send Query</button>
         <button onClick={fcl.unauthenticate}>Log Out</button>
       </div>
     );
